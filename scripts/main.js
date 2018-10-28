7bb099e5112da5d56f53752cb13297866dee44d4
Lucas <lucas.martinez.gd@gmail.com>
2018-10-28 12:20:00 +0100

    text now disappears when sidebar is collapsed

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,24 @@
+//function called on 
 function toggleSidebar() {
     panel = document.getElementById("left-panel");
     
     panel.style.width = panel.style.width == "var(--sidebar-width)" ? "0px" : "var(--sidebar-width)";
+    
+    //hide text if sidebar is collapsed
+    if (panel.style.width == "0px") {
+        panel.style.setProperty("--text-display", "none");
+
+    } else {
+        panel.style.setProperty("--text-display", "block");
+
+
+
+    }
 }
 
+
+
+//clearing HTML style attributes on sidebar when window is big enough
 function clearStyle() {
     e = document.getElementById("left-panel");
 
